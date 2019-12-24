91c299d81d22279304ef661250ea796e7eceb5e3
John Foley <jjfoley@smith.edu>
2019-12-24 09:51:51 -0500

    Simulation now supports generating JsonSchema files

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -70,4 +70,9 @@ pub trait Simulation {
 
     /// Legal action set:
     fn legal_action_set(&self) -> Vec<AleAction>;
+
+    /// Getter for JSON Schema for this game's state.
+    fn schema_for_state(&self) -> String;
+    /// Getter for JSON Schema for this game's config.
+    fn schema_for_config(&self) -> String;
 }
